0e38e0c7c773827b2f4cf582d800b666e3bf6395
David Latimore <dgl@u.northwestern.edu>
2017-04-06 22:14:52 -0500

    Turned off pause on hover feature

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,6 @@ $(function () {
         duration: 5000,
         duplicate: true,
         delayBeforeStart: 0,
-        pauseOnHover: true
+        pauseOnHover: false // toggle this to true to make the ticker stop when you hover over it
     });
 });
